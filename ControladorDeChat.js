75df1a71224bc30dc8aab1c8620410e575f9c4cb
Alejandro Giorgi <alejandro.giorgi@grupoesfera.com.ar>
2025-02-24 09:53:36 -0300

    Se encarga de manejar el chat

---
+++ ControladorDeChat.js
@@ -0,0 +1,53 @@
+const { WebSocketServer } = require("ws");
+
+const puerto = 8081;
+const CLIENTE_CONECTADO = 'connection';
+const MENSAJE_RECIBIDO = 'message';
+const MENSAJE_FIN = "[FIN]";
+
+class ControladorDeChat {
+    constructor(asistente) {
+        this.server = new WebSocketServer({port: puerto});
+        this.chats = new Map();
+        this.asistente = asistente;
+    }
+
+    iniciar() {
+        this.server.on(CLIENTE_CONECTADO, this.alConectarseUnCliente.bind(this));
+    }
+
+    alConectarseUnCliente(webSocket) {
+        console.log("[Controlador de chat] Cliente conectado");
+        webSocket.on(MENSAJE_RECIBIDO, (datos) => this.alRecibirUnMensaje(webSocket, datos));
+    }
+
+    alRecibirUnMensaje = async (webSocket, datos) => {
+        const mensaje = datos.toString();
+        console.log("[Controlador de chat] Mensaje recibido: ", mensaje);
+
+        let chat = await this.obtenerOCrearChat(webSocket)
+
+        const respuesta = await this.asistente.atender(chat, mensaje);
+        webSocket.send(respuesta);
+        this.finalizar(webSocket);
+    }
+
+    async obtenerOCrearChat(webSocket) {
+        let chat;
+        if (this.chats.has(webSocket)) {
+            chat = this.chats.get(webSocket);
+        } else {
+            const hilo = await this.asistente.crearHilo();
+            chat = hilo.id;
+            this.chats.set(webSocket, chat);
+        }
+
+        return chat;
+    }
+
+    finalizar(webSocket) {
+        webSocket.send(MENSAJE_FIN);
+    }
+}
+
+module.exports = ControladorDeChat;
