02a799f51ecf98a9fd5ccbdf574b5091a1a0b6cb
Alejandro Giorgi <alejandro.giorgi@grupoesfera.com.ar>
2025-02-25 11:31:02 -0300

    Se obtiene el id del asistente de una variable de entorno

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const Asistente = require("./Asistente");
 const app = express();
 const PORT = 3000;
 
-const ASSISTANT_ID = "asst_A8w6kRTELYi9flRMFLjvkPo2";
-
 app.use(cors());
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 
-    let chat = new ControladorDeChat(new Asistente(process.env.OPENAI_API_KEY, ASSISTANT_ID));
+    let chat = new ControladorDeChat(new Asistente(process.env.OPENAI_API_KEY, process.env.ASSISTANT_ID));
     chat.iniciar();
 });
